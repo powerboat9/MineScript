51a74931e068737b7781253d922ed04f872dbcf9
powerboat9 <powerboat9.gamer@gmail.com>
2017-01-30 02:25:37 -0500

    Added functions to escape commands and create falling sand entity data based on command and information about the command block

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-function encapsulate(cmd) {
+function cmdEscape(s) {
+    return s.replace(/\\/g, "\\\\").replace(/([\"'])/g, "\\$1");
+}
+
+function encapsulateFallingSand(cmd, insertIDTag, mode, hasConditional, direction) {
+    var s = "{";
+    if (insertIDTag) {
+        s += "id:\"minecraft:falling_block\",";
+    }
+    s += "Time:1,Block:\"minecraft:";
+    if (mode == 1) {
+        s += "repeating_";
+    } else if (mode == 2) {
+        s += "chain_";
+    } else {
+        return -1;
+    }
+    s += "command_block\",Data:";
+    var data = direction;
+    if (hasConditional) {
+        data += 8;
+    }
+    s += data;
+    s += ",TileEntityData:{Command:\"";
+    s += cmdEscape(cmd);
+    s += "\"}}";
+    return s;
+}
